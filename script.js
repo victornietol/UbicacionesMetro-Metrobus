8dbe1f25d9e57a40290026661938c900b94878bc
victornietol <nietovirgo17@gmail.com>
2025-07-18 18:12:32 -0600

    Se agrego logica para el funcionamiento del boton para buscar ubicacion introducidas en el input

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+// Mostrar u ocultar circulo de carga
+function showSpinner(state) {
+    if(state) {
+        document.getElementById("spinner_loading").innerHTML = `
+            <div id="contenedor" class="d-flex justify-content-center align-items-center" style="height: 100px;">
+                <div class="spinner-border text-primary" role="status"></div>
+            </div>
+        `;
+        document.getElementById("error_current_ubi").innerText = "Cargando...";
+    } else {
+        document.getElementById("spinner_loading").innerHTML = "";
+        document.getElementById("error_current_ubi").innerText = "";
+    }
+}
+
 const options = {
     enableHighAccuracy: true,
     timeout: 5000,
@@ -66,6 +81,7 @@ function updateMarkers(newCoords, markerGroup, map) { // newCoords debe ser un a
 let map;
 let markerGroup;
 async function initMap() {
+    showSpinner(true);
     const currLocation = await getCurrentLocation();
     if (!currLocation) {
         // El error se indica en la funcion error
@@ -77,6 +93,7 @@ async function initMap() {
     }).addTo(map);
     markerGroup = L.layerGroup().addTo(map);
     addMarker(currLocation.lat, currLocation.lon, "<b>Tu ubicación</b>", map, markerGroup);
+    showSpinner(false);
 }
 
 // Actualizar posicion de la camara o visualizacion del mapa
@@ -86,6 +103,7 @@ function updateViewPositionMap(latitude, longitude) {
 
 // Actualizar ubicacion actual
 async function updateCurrentLocation() {
+    showSpinner(true);
     const currLocation = await getCurrentLocation();
     if (!currLocation) {
         // El error se indica en la funcion error
@@ -100,6 +118,43 @@ async function updateCurrentLocation() {
     ]
     updateMarkers(newCoords, markerGroup, map); // Actualizar markers
     updateViewPositionMap(currLocation.lat, currLocation.lon);
+    showSpinner(false);
+}
+
+// Obtiene las coordenadas de la ubicacion indicada en el input
+async function getInputLocation(inputLocation) {
+    const url = `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(inputLocation)}`;
+
+     try {
+        const res = await fetch(url);
+        const data = await res.json();
+
+        if (data.length > 0) {
+            return { lat: data[0].lat, lon: data[0].lon };
+        } else {
+            return { lat: "", lon: "" };
+        }
+    } catch (error) {
+        document.getElementById("error_current_ubi").innerText = "Error al buscar la ubicación";
+        return { lat: "Error en la solicitud", lon: "Error en la solicitud" };
+    }
+}
+
+async function showInputLocation() {
+    showSpinner(true);
+    const inputLocation = document.getElementById("inputLocation").value;
+    const inputCoords = await getInputLocation(inputLocation);
+    const newCoords = [
+        {
+            latitude: inputCoords.lat,
+            longitude: inputCoords.lon,
+            textPopup: "<b>Ubicación aproximada búscada.</b>"
+        },
+        // Aqui agregar las estaciones de transporte necesarias o puedo crearlas todas al inicio
+    ];
+    updateMarkers(newCoords, markerGroup, map);
+    updateViewPositionMap(inputCoords.lat, inputCoords.lon);
+    showSpinner(false);
 }
 
 
